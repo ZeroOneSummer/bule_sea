1b725c693e4a70e73d57866b8c2806b65a50795f
zeroonesummer <592706328@qq.com>
2020-05-04 20:53:46 +0800

    四叶草，棒棒棒!

--- nweb/src/main/resources/static/js/index.js
+++ nweb/src/main/resources/static/js/index.js
@@ -2,14 +2,14 @@
 $(function () {
     //奇数行点击事件
     $("li:odd").click(function () {
-        confirm("确定变成红色吗");
-        this.css("background: red");
+        confirm("奇数行变成红色吗");
+        $("li:odd").css("background", "red");
     });
 
     //偶数行点击事件
     $("li:even").click(function () {
-        confirm("确定变成红灰色吗");
-        this.css("background: gray");
+        confirm("偶数行变成灰色吗");
+        $("li:even").css("background", "gray");
     });
 });
 
